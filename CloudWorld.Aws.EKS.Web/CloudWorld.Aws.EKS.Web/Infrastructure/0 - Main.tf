549465612de220f2a60b6ab0cc930798681bb514
Santiago Arango Toro <39422147+CloudWorld-Tech@users.noreply.github.com>
2025-04-15 20:14:22 -0500

    Added ACR Update Dockerfile Update YAMLS

--- CloudWorld.Aws.EKS.Web/CloudWorld.Aws.EKS.Web/Infrastructure/0 - Main.tf
+++ CloudWorld.Aws.EKS.Web/CloudWorld.Aws.EKS.Web/Infrastructure/0 - Main.tf
@@ -7,4 +7,6 @@ terraform {
   }
 }
 provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
 }
---
+++ CloudWorld.Aws.EKS.Web/CloudWorld.Aws.EKS.Web/Infrastructure/6 - Ecr.tf
@@ -0,0 +1,4 @@
+﻿resource "aws_ecrpublic_repository" "cloud-world-ecr" {
+  repository_name = "cloud-world-ecr/apps"
+  provider = aws.us_east_1
+}
